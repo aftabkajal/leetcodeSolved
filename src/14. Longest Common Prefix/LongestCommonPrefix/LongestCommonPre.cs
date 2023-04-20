f6397d03bdb64d10f77fff7dee4d39d67cb12f82
Aftab Uddin Kajal <kajal.alpha@gmail.com>
2023-04-20 14:48:43 +0600

    14. Longest Common Prefix solved

---
+++ src/14. Longest Common Prefix/LongestCommonPrefix/LongestCommonPre.cs
@@ -0,0 +1,25 @@
+﻿namespace LeetCodeSolution
+{
+    public class LongestCommonPre
+    {
+        public static string CommonPrefix(string[] strs)
+        {
+            var commonString = string.Empty;
+            for (var i = 1; i <= strs.First().Length; i++)
+            {
+                var sbstring = strs[0].Substring(0, i);
+                if (strs.All(a => a.StartsWith(sbstring)))
+                {
+                    commonString = sbstring;
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            return commonString;
+        }
+    }
+    
+}
---
+++ src/14. Longest Common Prefix/LongestCommonPrefix/Program.cs
@@ -0,0 +1,13 @@
+﻿
+namespace LeetCodeSolution
+{
+    public class LongestCommonprSln
+    {
+        static void Main(string[] args)
+        {
+           string[] strs = { "flower", "flow", "flight" };
+            LongestCommonPre.CommonPrefix(strs);
+            Console.WriteLine(args.Length);
+        }
+    }
+}

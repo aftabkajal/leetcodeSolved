65c3c03d1fcc8d484a8e3b4aeb5de9a7c3c2295d
Aftab Uddin Kajal <kajal.alpha@gmail.com>
2023-04-21 13:38:42 +0600

    FirstFactorial solved

---
+++ src/14. Longest Common Prefix/LongestCommonPrefix/CoderByte/FirstFactorial.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class FirstFactorialMainClass
+{
+
+    public static int FirstFactorial(int num)
+    {
+
+        var factorial = 1;
+        for (var i = 1; i <= num; i++)
+        {
+            factorial *= i;
+        }
+        return factorial;
+
+    }
+
+    static void Main()
+    {
+
+        // keep this function call here
+        Console.WriteLine(FirstFactorial(Console.ReadLine()));
+
+    }
+
+}

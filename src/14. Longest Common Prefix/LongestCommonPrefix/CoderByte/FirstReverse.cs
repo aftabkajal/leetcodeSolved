297a77706c5ac5e9cab4e2fc66ac00a6b6fc35ce
Aftab Uddin Kajal <kajal.alpha@gmail.com>
2023-04-21 13:19:30 +0600

    FirstReverse provlem solved in coderbyte

---
+++ src/14. Longest Common Prefix/LongestCommonPrefix/CoderByte/FirstReverse.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text;
+
+class MainClass
+{
+
+    public static string FirstReverse(string str)
+    {
+        if (str == null) return str;
+
+        StringBuilder stringOUTPUT = new StringBuilder();
+        int length = str.Length - 1;
+        for (int i = length; i < str.Length; i--)
+        {
+            if (i < 0) break;
+            stringOUTPUT.Append(str[i]);
+        }
+
+
+        return stringOUTPUT.ToString();
+
+    }
+
+    static void Main()
+    {
+
+        // keep this function call here
+        Console.WriteLine(FirstReverse("Hello World and Coders"));
+
+    }
+
+}
